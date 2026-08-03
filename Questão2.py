f252aa9f5c79561c15b0c9debd7f09afdc07fbf2
thamirisouza <thagira@hotmail.com>
2026-08-03 19:21:10 -0300

    Create Questão2.py

---
+++ Questão2.py
@@ -0,0 +1,30 @@
+'''
+Questão 2 – Identificando tipos de dados 
+
+Crie variáveis contendo:  
+
+ - Um texto
+ - Um número inteiro
+ - Um número decimal
+ - Um valor booleano 
+
+Depois, utilize a função type() para exibir o tipo de cada variável. 
+
+Exemplo de saída: 
+
+<class 'str'> 
+<class 'int'>
+<class 'float'> 
+<class 'bool'> 
+'''
+
+texto = 'Coisa'
+inteiro = 1
+decimal = 1.0
+booleano = True
+
+print(type(texto))
+print(type(inteiro))
+print(type(decimal))
+print(type(booleano))
+
