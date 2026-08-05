17ed8dcc7ebd54c69c917744c37fe159ad7e623c
thamirisouza <thagira@hotmail.com>
2026-08-05 20:00:25 -0300

    Create Materiais.py

---
+++ 2026.08.05 Dicionários/Materiais.py
@@ -0,0 +1,38 @@
+''' Criar coleção de materiais '''
+
+produtos = [{"nome": "Argamassa", 
+            "codigo" : "A001",
+            "preco" : 50.00,
+            "quantidade" : 10,
+            "categoria" : "Materiais básicos"
+            },
+
+            {"nome": "Porta", 
+            "codigo" : "P002",
+            "preco" : 450.00,  
+            "quantidade" : 5,
+            "categoria" : "Marcenária"
+           },
+
+            {"nome": "Tinta",
+            "codigo" : "T003",
+            "preco" : 25.00,   
+            "quantidade" : 20,
+            "categoria" : "Pintura"
+           } ,
+
+            {"nome": "Furadeira",
+            "codigo" : "F004",    
+            "preco" : 150.00,
+            "quantidade" : 8,
+            "categoria" : "Ferramentas"
+           },
+]  
+
+# Todos os produtos
+print(produtos)
+
+
+
+
+
