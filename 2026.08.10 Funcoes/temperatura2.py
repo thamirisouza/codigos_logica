09fb3cf4d7ed87c1a49fc90c6a39c67935bbfb9e
thamirisouza <thagira@hotmail.com>
2026-08-10 20:28:56 -0300

    Create temperatura2.py

---
+++ 2026.08.10 Funcoes/temperatura2.py
@@ -0,0 +1,7 @@
+def fahrenheit_to_celsius(fahrenheit):
+  return (fahrenheit - 32) * 5 / 9
+  print("Acabou")
+
+print(fahrenheit_to_celsius(77))
+print(fahrenheit_to_celsius(95))
+print(fahrenheit_to_celsius(50))
