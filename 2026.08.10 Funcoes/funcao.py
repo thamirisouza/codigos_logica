2aa0ceecfad39ba7e23cfc8bfdf26852e5c3a121
thamirisouza <thagira@hotmail.com>
2026-08-10 20:28:46 -0300

    Create funcao.py

---
+++ 2026.08.10 Funcoes/funcao.py
@@ -0,0 +1,12 @@
+def my_function():
+  print("Hello from a function")
+  a = 20
+  if 10 == a:
+    print("É igual")
+  else:
+    print("É diferente")
+
+my_function()
+my_function()
+my_function()
+my_function()
