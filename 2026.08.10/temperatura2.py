9cf74fee7ac17e0046ea93120c84ed366cd096be
thamirisouza <thagira@hotmail.com>
2026-08-10 20:29:12 -0300

    Delete temperatura2.py

--- 2026.08.10/temperatura2.py
+++
@@ -1,7 +0,0 @@
-def fahrenheit_to_celsius(fahrenheit):
-  return (fahrenheit - 32) * 5 / 9
-  print("Acabou")
-
-print(fahrenheit_to_celsius(77))
-print(fahrenheit_to_celsius(95))
-print(fahrenheit_to_celsius(50))
