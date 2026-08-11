93cb4ebf8f80605eaf122c4ccbb751fbbf46e588
thamirisouza <thagira@hotmail.com>
2026-08-10 21:02:18 -0300

    Update agenda.py

--- 2026.08.10 Funcoes/agenda.py
+++ 2026.08.10 Funcoes/agenda.py
@@ -4,6 +4,12 @@
 # By Luferat - https://github.xonm/Luferat #
 ############################################
 
+
+import subprocess
+
+def cls():
+    subprocess.run("cls", shell=True)
+
 def new_contact():
     pass
 
@@ -17,4 +23,41 @@ def delete_contact():
     pass
 
 # Programa principal
-def main():
+def main(erro = str()):
+    # Main loop
+    while True:
+        cls() 
+        print("[ AGENDA FURRECA - MENU PRINCIPAL ]")
+        print(erro)
+        erro = str()
+        print('''
+Opções:
+
+1 - Novo contato
+2 - Listar contatos
+3 - Editar contato
+4 - Apagar contato
+0 - Sair do programa
+    ''')
+        
+        opcao = input("Escolha uma opção: ")
+
+        match int(opcao):
+            case 1:
+                new_contact()
+            case 2:
+                list_contacts()
+            case 3:
+                edit_contact()
+            case 4:
+                delete_contact()
+            case 0:
+                cls()
+                print("\nAcabou!")
+                exit()
+            case _:
+                erro = "Digite uma opção válida!"
+                main(erro)
+
+# "Roda" o programa
+main()     
